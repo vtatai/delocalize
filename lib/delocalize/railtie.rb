55b21755b6e9ce3a6d087f09297a3afe56607757
Victor Tatai <vtatai@gmail.com>
2012-05-26 12:37:33 -0300

    Removing railtie hook which does not work with mongoid

--- lib/delocalize/railtie.rb
+++ lib/delocalize/railtie.rb
@@ -3,12 +3,10 @@ require 'delocalize/delocalizable'
 module Delocalize
   class Railtie < Rails::Railtie
     initializer "delocalize" do |app|
+      # ActiveRecord supported by default
+      # To use mongoid use setup or manually include the Delocalizable module in your class
       ActiveSupport.on_load :active_record do
         ActiveRecord::Base.send(:include, Delocalize::Delocalizable)
-      end
-
-      ActiveSupport.on_load :mongoid do
-        Mongoid::Document.send(:include, Delocalize::Delocalizable)
       end
 
       ActiveSupport.on_load :action_view do
